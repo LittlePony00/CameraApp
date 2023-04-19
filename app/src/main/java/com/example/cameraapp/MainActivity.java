a353a5dee18cf94983569781b305d0478c8ccc9c
1.kla$ <recum12233@mail.ru>
2023-04-20 02:03:57 +0300

    ГОВНОКОД!!!!

--- app/src/main/java/com/example/cameraapp/MainActivity.java
+++ app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -21,8 +21,13 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Rational;
+import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,9 +39,23 @@ import android.widget.Toast;
 
 import com.example.cameraapp.DB.Photos;
 import com.example.cameraapp.DB.PhotosDB;
+import com.example.cameraapp.ml.Model;
 import com.google.common.util.concurrent.ListenableFuture;
 
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
@@ -83,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
             startCamera(cameraFacing);
         }
     }
+
     public void startCamera(int cameraFacing) {
         int aspectRatio = aspectRatio(imageView.getWidth(), imageView.getHeight());
         ListenableFuture<ProcessCameraProvider> listenableFuture = ProcessCameraProvider.getInstance(this);
@@ -94,7 +114,9 @@ public class MainActivity extends AppCompatActivity {
                 Preview preview = new Preview.Builder().setTargetAspectRatio(aspectRatio).build();
 
                 ImageCapture imageCapture = new ImageCapture.Builder().setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-                        .setTargetRotation(getWindowManager().getDefaultDisplay().getRotation()).build();
+                        //.setTargetRotation(getWindowManager().getDefaultDisplay().getRotation())
+                        .setTargetRotation(Surface.ROTATION_90)
+                        .build();
 
                 CameraSelector cameraSelector = new CameraSelector.Builder()
                         .requireLensFacing(cameraFacing).build();
@@ -105,6 +127,7 @@ public class MainActivity extends AppCompatActivity {
 
                 button.setOnClickListener(view -> {
                     takePicture(imageCapture);
+
                 });
                 preview.setSurfaceProvider(imageView.getSurfaceProvider());
             } catch (ExecutionException | InterruptedException e) {
@@ -122,11 +145,15 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(MainActivity.this, "Image saved at: " + file.getPath(), Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(MainActivity.this, "Image saved at: " + file.getPath(), Toast.LENGTH_SHORT).show();
                         Photos photos = new Photos();
                         photos.path = file.getPath();
                         photos.result = "Nothing";
-
+                        try {
+                            Toast.makeText(MainActivity.this, "DAS IST " + processImage(file.getAbsoluteFile()), Toast.LENGTH_SHORT).show();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
                         db.photosDAO().insert(photos);
                     }
                 });
@@ -146,11 +173,113 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
     private int aspectRatio(int width, int height) {
         double previewRatio = (double) Math.max(width, height) / Math.min(width, height);
         if (Math.abs(previewRatio - 4.0 / 3.0) <= Math.abs(previewRatio - 16.0 / 9.0)) {
             return AspectRatio.RATIO_4_3;
         }
         return AspectRatio.RATIO_16_9;
+    }
+
+    public String processImage(File image) throws IOException {
+        try {
+            Model model = Model.newInstance(getApplicationContext());
+            Bitmap originalBitmap = null;
+            FileInputStream stream = new FileInputStream(image);
+            originalBitmap = BitmapFactory.decodeStream(stream);
+            stream.close();
+
+            // Обрезаем bitmap до соотношения сторон 3:4
+            Bitmap resizedBitmap = null;
+            int originalWidth = originalBitmap.getWidth();
+            int originalHeight = originalBitmap.getHeight();
+            float targetRatio = 3f / 4f;
+            float originalRatio = (float) originalWidth / originalHeight;
+
+            File file1 = new File(getExternalFilesDir(null), System.currentTimeMillis() + "Original" + ".jpg");
+
+            // Открываем поток для записи изображения в файл
+            OutputStream outputStream1 = new FileOutputStream(file1);
+
+            // Конвертируем Bitmap в JPEG и записываем в поток
+            originalBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream1);
+
+            // Закрываем поток
+            outputStream1.close();
+            if (originalRatio > targetRatio) {
+                // нужно обрезать по горизонтали
+                int targetWidth = (int) (originalHeight * targetRatio);
+                int startX = (originalWidth - targetWidth) / 2;
+                resizedBitmap = Bitmap.createBitmap(originalBitmap, startX, 0, targetWidth, originalHeight);
+            } else {
+                // нужно обрезать по вертикали
+                int targetHeight = (int) (originalWidth / targetRatio);
+                int startY = (originalHeight - targetHeight) / 2;
+                resizedBitmap = Bitmap.createBitmap(originalBitmap, 0, startY, originalWidth, targetHeight);
+            }
+            File file = new File(getExternalFilesDir(null), System.currentTimeMillis() + "beforeResize" + ".jpg");
+
+            // Открываем поток для записи изображения в файл
+            OutputStream outputStream = new FileOutputStream(file);
+
+            // Конвертируем Bitmap в JPEG и записываем в поток
+            resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+
+            // Закрываем поток
+            outputStream.close();
+            resizedBitmap = Bitmap.createScaledBitmap(resizedBitmap, 90, 120, true);
+
+            File file2 = new File(getExternalFilesDir(null), System.currentTimeMillis() + ".jpg");
+
+            // Открываем поток для записи изображения в файл
+            OutputStream outputStream2 = new FileOutputStream(file2);
+
+            // Конвертируем Bitmap в JPEG и записываем в поток
+            resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream2);
+
+            // Закрываем поток
+            outputStream2.close();
+
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 90, 120, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * 90 * 120 * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            int[] intValues = new int[90 * 120];
+            resizedBitmap.getPixels(intValues, 0, resizedBitmap.getWidth(), 0, 0, resizedBitmap.getWidth(), resizedBitmap.getHeight());
+
+            int pix = 0;
+            for (int i = 0; i < 90; i++) {
+                for (int j = 0; j < 120; j++) {
+                    int val = intValues[pix++];
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255));
+                    byteBuffer.putFloat((val & 0xFF) * (1.f / 255));
+                }
+            }
+
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            Model.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+            float[] confidence = outputFeature0.getFloatArray();
+            int maxPos = 0;
+            float maxConfidence = 0;
+            for (int i = 0; i < confidence.length; i++) {
+                if (confidence[i] > maxConfidence) {
+                    maxConfidence = confidence[i];
+                    maxPos = i;
+                }
+            }
+            String[] classes = {"Dress", "Hat", "Pants", "Shirt", "Shoes", "Shorts", "Skirt", "Sweater", "TShirt"};
+            model.close();
+            return classes[maxPos];
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+        return "";
     }
 }
